ff02069a51e4b3639f6b275a3abf7badccec2c1b
Binit Sapkota <mrbinit@Binits-MacBook-Air.local>
2023-09-08 17:17:45 +0300

    first commit

--- server/Dataimport.js
+++ server/Dataimport.js
@@ -4,7 +4,7 @@ import users from "./data/users.js";
 
 const ImportData = express.Router();
 
-ImportData.post("user", async(req, res) => {
+ImportData.post("/user", async(req, res) => {
     await User.remove({});
     const importUser = await User.insertMany(users);
     res.send({ importUser });
--- server/server.js
+++ server/server.js
@@ -257,7 +257,7 @@
 // })
 
 import express from "express";
-import products from "./data/Products.js";
+//import products from "./data/Products.js";
 import dotenv from "dotenv"
 import connectDatabase from "./config/MongoDb.js";
 import ImportData from "./Dataimport.js";
@@ -269,13 +269,13 @@ const app = express();
 
 
 
-app.get("/api/products", (req, res) => {
-    res.json(products)
-});
+// app.get("/api/products", (req, res) => {
+//     res.json(products)
+// });
 
 
 //API
-// app.use("/api/import", ImportData);
+app.use("/api/import", ImportData);
 
 
 app.get("/", (req, res) => {
@@ -283,4 +283,4 @@ app.get("/", (req, res) => {
 });
 const PORT = process.env.PORT || 1000;
 
-app.listen(PORT, console.log("Server running on port 3000"));
+app.listen(PORT, console.log(`Server running on port ${PORT}`));
