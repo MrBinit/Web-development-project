70b29020f983eaf95eb3c208f842e7555cdb4ae7
Binit Sapkota <mrbinit@Binits-MacBook-Air.local>
2023-09-09 20:45:57 +0300

    first commit

--- server/Dataimport.js
+++ server/Dataimport.js
@@ -3,22 +3,41 @@ import User from "./Models/UserModel.js";
 import users from "./data/users.js";
 import products from "./data/Products.js";
 import Product from "./Models/ProductModel.js";
+import asyncHandler from "express-async-handler";
 
 const ImportData = express.Router();
 
-ImportData.post("/user", async(req, res) => {
-    await User.deleteMany({});
-    const importUser = await User.insertMany(users);
-    res.send({ importUser });
+// ImportData.post("/user", async(req, res) => {
+//     await User.deleteMany({});
+//     const importUser = await User.insertMany(users);
+//     res.send({ importUser });
+//     // res.send("hello world");
+// });
+ImportData.post(
+    "/user", 
+    asyncHandler(async(req, res) => {
+        await User.deleteMany({});
+        const importUser = await User.insertMany(users);
+        res.send({ importUser });
+    })
     // res.send("hello world");
-});
+);
 
-ImportData.post("/products", async(req, res) => {
-    await Product.deleteMany({});
-    const importProducts = await Product.insertMany(products);
-    res.send({ importProducts });
-    // res.send("hello world");
-});
+// ImportData.post("/products", async(req, res) => {
+//     await Product.deleteMany({});
+//     const importProducts = await Product.insertMany(products);
+//     res.send({ importProducts });
+//     // res.send("hello world");
+// });
+ImportData.post(
+    "/products", 
+    asyncHandler (async(req, res) => {
+        await Product.deleteMany({});
+        const importProducts = await Product.insertMany(products);
+        res.send({ importProducts });
+        // res.send("hello world");
+    })
+);
 
 export default ImportData; 
 
