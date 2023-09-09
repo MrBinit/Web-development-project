052f3d017475862f3cc0ddeb14b03c30a71c3ca5
Binit Sapkota <mrbinit@Binits-MacBook-Air.local>
2023-09-09 21:22:07 +0300

    first commit

--- server/server.js
+++ server/server.js
@@ -261,6 +261,8 @@ import express from "express";
 import dotenv from "dotenv"
 import connectDatabase from "./config/MongoDb.js";
 import ImportData from "./Dataimport.js";
+import productRoute from "./Routes/ProductRoutes.js";
+import { errorHandler, notFound } from "./Middleware/Errors.js";
 
 
 dotenv.config();
@@ -276,12 +278,15 @@ const app = express();
 
 //API
 app.use("/api/import", ImportData);
-// app.use("/api/products", productRoute);
+app.use("/api/products", productRoute);
 
+//Error Handler
+app.use(notFound);
+app.use(errorHandler);
 
-app.get("/", (req, res) => {
-    res.send("WelcomeAPI is running...... Hello These is Binit");
-});
+// app.get("/", (req, res) => {
+//     res.send("WelcomeAPI is running...... Hello These is Binit");
+// });
 const PORT = process.env.PORT || 1000;
 
 app.listen(PORT, console.log(`Server running on port ${PORT}`));
